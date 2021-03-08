5a7c0ce10bb81018a2b60ae0238beec2d917b646
dikt-prole <dikt-prole@mail.ru>
2021-03-08 04:04:32 +0300

    last 10 lenin books - data + pages

--- LeninSearch.Core/FileData.cs
+++ LeninSearch.Core/FileData.cs
@@ -6,7 +6,6 @@ namespace LeninSearch.Core
 {
     public class FileData
     {
-        public Dictionary<ushort, string> Headers { get; set; }
         public List<Heading> Headings { get; set; }
         public Dictionary<ushort, ushort> Pages { get; set; }
         public List<Paragraph> Pars { get; set; }
