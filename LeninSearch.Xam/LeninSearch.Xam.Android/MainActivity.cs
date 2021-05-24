53fc30601121163e4f221b5682f925bbf98a2bb9
dikt-prole <75211373+dikt-prole@users.noreply.github.com>
2021-05-24 05:38:38 -0700

    Implemented splash screen

--- LeninSearch.Xam/LeninSearch.Xam.Android/MainActivity.cs
+++ LeninSearch.Xam/LeninSearch.Xam.Android/MainActivity.cs
@@ -22,11 +22,11 @@ using System.Linq;
 
 namespace LeninSearch.Xam.Droid
 {
-    [Activity(Label = "LeninSearch.Xam", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "LeninSearch.Xam", Icon = "@drawable/icon", Theme = "@style/MainTheme"/*, MainLauncher = true*/, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         private App _app;
-        private GlobalEvents _globalEvents;        
+        private GlobalEvents _globalEvents;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -43,61 +43,12 @@ namespace LeninSearch.Xam.Droid
 
             ReportItem.GlobalDeviceId = Android.Provider.Settings.Secure.GetString(Application.Context.ContentResolver, Android.Provider.Settings.Secure.AndroidId);
 
-            State.ClearCorpusData();
-
-            using (var reader = new StreamReader(Assets.Open("main.json")))
-            {
-                var response = reader.ReadToEnd();
-                var corpus = JsonConvert.DeserializeObject<Corpus>(response);
-                State.AddCorpus(corpus);
-
-                var targetCorpusFile = $"{FileUtil.CorpusFolder}/main.json";
-                var needUnzip = !File.Exists(targetCorpusFile) ||
-                                JsonConvert.DeserializeObject<Corpus>(File.ReadAllText(targetCorpusFile)).Version !=
-                                corpus.Version;
-                if (needUnzip)
-                {
-                    // 1. unzip
-                    if (Directory.Exists(FileUtil.CorpusFolder))
-                    {
-                        Directory.Delete(FileUtil.CorpusFolder, true);
-                    }
-                    UnzipAsset("main.zip", FileUtil.CorpusFolder);
-
-                    // 2. index
-                    var lsFiles = Directory.GetFiles(FileUtil.CorpusFolder, "*.ls");
-                    ConcurrentOptions.OneByOne = Build.VERSION.SdkInt == BuildVersionCodes.LollipopMr1;
-                    if (ConcurrentOptions.OneByOne)
-                    {
-                        foreach (var lsFile in lsFiles) ConvertLsToLsi(lsFile);
-                    }
-                    else
-                    {
-                        for (var i = 0; i < lsFiles.Length; i += ConcurrentOptions.LsToLsiBatchSize)
-                        {
-                            var tasks = lsFiles.Skip(i).Take(ConcurrentOptions.LsToLsiBatchSize).Select(lsFile => Task.Run(() => ConvertLsToLsi(lsFile))).ToArray();
-                            Task.WaitAll(tasks);
-                        }                        
-                    }                    
-                }
-            }
-
             Settings.OldAndroidJustification = Build.VERSION.SdkInt < BuildVersionCodes.O;
 
             _globalEvents = new GlobalEvents();
             _app = new App(_globalEvents);
             LoadApplication(_app);
-        }
-
-        private static void ConvertLsToLsi(string lsFile)
-        {
-            var lsiFileName = lsFile.Replace(".ls", ".lsi");
-            var lsBytes = File.ReadAllBytes(lsFile);
-            var lsData = LsUtil.LoadOptimized(lsBytes, CancellationToken.None);
-            var lsiBytes = LsIndexUtil.ToLsIndexBytes(lsData);
-            File.WriteAllBytes(lsiFileName, lsiBytes);
-            File.Delete(lsFile);
-        }
+        }        
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -116,41 +67,6 @@ namespace LeninSearch.Xam.Droid
             {
                 _globalEvents.OnBackButtonPressed();
             }
-        }
-
-        public void UnzipAsset(string assetName, string destPath)
-        {
-            byte[] buffer = new byte[1024];
-            int byteCount;
-
-            var destPathDir = new Java.IO.File(destPath);
-            destPathDir.Mkdirs();
-
-            using (var assetStream = Assets.Open(assetName, Android.Content.Res.Access.Streaming))
-            {
-                using (var zipStream = new ZipInputStream(assetStream))
-                {
-                    ZipEntry zipEntry;
-                    while ((zipEntry = zipStream.NextEntry) != null)
-                    {
-                        if (zipEntry.IsDirectory)
-                        {
-                            var zipDir = new Java.IO.File(Path.Combine(destPath, zipEntry.Name));
-                            zipDir.Mkdirs();
-                            continue;
-                        }
-
-                        using (var fileStream = new FileStream(Path.Combine(destPath, zipEntry.Name), FileMode.CreateNew))
-                        {
-                            while ((byteCount = zipStream.Read(buffer)) != -1)
-                            {
-                                fileStream.Write(buffer, 0, byteCount);
-                            }
-                        }
-                        zipEntry.Dispose();
-                    }
-                }
-            }
-        }
+        }        
     }
 }
