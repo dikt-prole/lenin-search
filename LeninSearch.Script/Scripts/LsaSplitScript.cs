429277e2e4c19d9b72e3403c5c7f3f76719fdba0
dikt-prole <75211373+dikt-prole@users.noreply.github.com>
2022-02-17 15:35:48 +0300

    prepared test lsa script

---
+++ LeninSearch.Script/Scripts/LsaSplitScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using LeninSearch.Standard.Core.Corpus.Json;
+using Newtonsoft.Json;
+
+namespace LeninSearch.Script.Scripts
+{
+    public class LsaSplitScript : IScript
+    {
+        public string Id => "lsa-split-script";
+        public string Arguments => "json-file, split-folder";
+        public void Execute(params string[] input)
+        {
+            var jsonFile = input[0];
+            var splitFolder = input[1];
+
+            var jsonData = JsonConvert.DeserializeObject<JsonFileData>(File.ReadAllText(jsonFile));
+
+            for (var i = 0; i < jsonData.Pars.Count; i++)
+            {
+                var parText = jsonData.Pars[i].Text;
+
+                if (string.IsNullOrEmpty(parText)) continue;
+
+                var parFile = Path.Combine(splitFolder, $"{Path.GetFileNameWithoutExtension(jsonFile)}_{i}.txt");
+
+                File.WriteAllText(parFile, parText);
+            }
+        }
+    }
+}
--- LeninSearch.Script/Scripts/OcrJsonScript.cs
+++ LeninSearch.Script/Scripts/OcrJsonScript.cs
@@ -13,31 +13,33 @@ namespace LeninSearch.Script.Scripts
         public string Arguments => "book-folders";
         public void Execute(params string[] input)
         {
-            var bookFolders = input;
-            var blockService = new YandexVisionOcrLineService();
-            foreach (var bookFolder in bookFolders)
-            {
-                Console.WriteLine($"Processing book folder: {bookFolder}");
-                var sw = new Stopwatch(); sw.Start();
-                var ocrData = OcrData.Empty();
-                var imageFiles = Directory.GetFiles(bookFolder)
-                    .Where(f => f.EndsWith(".png") || f.EndsWith(".jpg") || f.EndsWith(".jpeg"));
-                foreach (var imageFile in imageFiles)
-                {
-                    var linesResult = blockService.GetOcrPageAsync(imageFile).Result;
-                    if (!linesResult.Success)
-                    {
-                        Console.WriteLine(linesResult.Error);
-                        return;
-                    }
+            throw new NotImplementedException();
 
-                    ocrData.Lines.AddRange(linesResult.Lines);
-                }
+            //var bookFolders = input;
+            //var blockService = new YandexVisionOcrLineService();
+            //foreach (var bookFolder in bookFolders)
+            //{
+            //    Console.WriteLine($"Processing book folder: {bookFolder}");
+            //    var sw = new Stopwatch(); sw.Start();
+            //    var ocrData = OcrData.Empty();
+            //    var imageFiles = Directory.GetFiles(bookFolder)
+            //        .Where(f => f.EndsWith(".png") || f.EndsWith(".jpg") || f.EndsWith(".jpeg"));
+            //    foreach (var imageFile in imageFiles)
+            //    {
+            //        var linesResult = blockService.GetOcrPageAsync(imageFile).Result;
+            //        if (!linesResult.Success)
+            //        {
+            //            Console.WriteLine(linesResult.Error);
+            //            return;
+            //        }
 
-                ocrData.Save(bookFolder);
-                sw.Stop();
-                Console.WriteLine($"Ready in {sw.Elapsed}");
-            }
+            //        ocrData.Lines.AddRange(linesResult.Lines);
+            //    }
+
+            //    ocrData.Save(bookFolder);
+            //    sw.Stop();
+            //    Console.WriteLine($"Ready in {sw.Elapsed}");
+            //}
         }
     }
 }
