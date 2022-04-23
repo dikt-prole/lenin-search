0d2384cdec1ce6a1eb2e3669414f6673744c1f82
dikt-prole <75211373+dikt-prole@users.noreply.github.com>
2022-04-23 17:45:18 +0300

    implemented ocr text to fb2 text (no images, no notes yet)

--- LeninSearch.Script/Scripts/Models/Fb2Line.cs
+++ LeninSearch.Script/Scripts/Models/Fb2Line.cs
@@ -1,13 +1,48 @@
-﻿namespace LeninSearch.Script.Scripts.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeninSearch.Ocr.Model;
+
+namespace LeninSearch.Script.Scripts.Models
 {
     public class Fb2Line
     {
-        public string Text { get; set; }
+        public int TopLeftY { get; set; }
+        public List<OcrWord> Words { get; set; }
         public Fb2LineType Type { get; set; }
+        public string GetText()
+        {
+            return string.Join(" ", Words.Select(w => w.Text));
+        }
+        public string GetXml()
+        {
+            switch (Type)
+            {
+                case Fb2LineType.Paragraph:
+                    return $"<p>{GetText()}</p>";
+                case Fb2LineType.Title:
+                    return $"<title><p>{GetText()}</p><title/>";
+            }
+
+            return null;
+        }
+
+        public static Fb2Line Construct(OcrLine line)
+        {
+            var fb2Line = new Fb2Line
+            {
+                Type = line.Label == OcrLabel.Title 
+                    ? Fb2LineType.Title 
+                    : Fb2LineType.Paragraph,
+                Words = line.Words.ToArray().ToList(),
+                TopLeftY = line.TopLeftY
+            };
+
+            return fb2Line;
+        }
     }
 
     public enum Fb2LineType
     {
-        Paragraph, Image
+        Paragraph, Image, Title
     }
 }
--- LeninSearch.Script/Scripts/OcrToFb2Script.cs
+++ LeninSearch.Script/Scripts/OcrToFb2Script.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using LeninSearch.Ocr.Model;
 using LeninSearch.Script.Scripts.Models;
 
@@ -10,28 +13,68 @@ namespace LeninSearch.Script.Scripts
         public string Arguments => "ocr-book-folder, fb2-path";
         public void Execute(params string[] input)
         {
-            var ocrBookFolder = input[0];
-            var fb2Path = input[1];
+            //var ocrBookFolder = input[0];
+            //var fb2Path = input[1];
+            var ocrBookFolder = @"D:\Repo\lenin-search\corpus\ocr\pavlov-v1\t01";
+            var fb2Path = @"D:\Repo\lenin-search\corpus\orig\pavlov\t01.fb2";
 
             // 1. load ocr data
             var ocrData = OcrData.Load(ocrBookFolder);
 
             // 2. go through pages and create sections, notes & images
             var sections = new List<List<Fb2Line>>();
-            var images = new List<string>();
             var notes = new List<string>();
-            foreach (var page in ocrData.Pages)
+            for (var pageIndex = 0; pageIndex < ocrData.Pages.Count; pageIndex++)
             {
-                var pageParagraphs = new List<Fb2Line>();
-                var pageImages = new List<Fb2Line>();
+                var page = ocrData.Pages[pageIndex];
+                var pageParagraphLinesFb2 = new List<Fb2Line>();
+                var pageImageLinesFb2 = new List<Fb2Line>();
+
+                var paragraphOcrLines = page.Lines.Where(l => l.Label == OcrLabel.PStart || l.Label == OcrLabel.PMiddle || l.Label == OcrLabel.Title).ToList();
+                if (paragraphOcrLines.Any())
+                {
+                    var currentParagraphLineFb2 = Fb2Line.Construct(paragraphOcrLines[0]);
+                    pageParagraphLinesFb2.Add(currentParagraphLineFb2);
+
+                    for (var paragraphOcrLineIndex = 1; paragraphOcrLineIndex < paragraphOcrLines.Count; paragraphOcrLineIndex++)
+                    {
+                        var currentLine = paragraphOcrLines[paragraphOcrLineIndex];
+
+                        if (currentLine.Label == OcrLabel.PStart)
+                        {
+                            currentParagraphLineFb2 = Fb2Line.Construct(currentLine);
+                            pageParagraphLinesFb2.Add(currentParagraphLineFb2);
+                        }
+                        else if (currentLine.Label == OcrLabel.Title)
+                        {
+                            pageParagraphLinesFb2.Add(Fb2Line.Construct(currentLine));
+                            currentParagraphLineFb2 = null;
+                        }
+                        else
+                        {
+                            if (currentParagraphLineFb2 == null)
+                                throw new Exception($"Page {pageIndex}: expected not null current paragraph line. Check that paragraph start goes after title");
+
+                            currentParagraphLineFb2.Words.AddRange(currentLine.Words);
+                        }
+                    }
+                }
+
+                var section = pageParagraphLinesFb2.Concat(pageImageLinesFb2).OrderBy(l => l.TopLeftY).ToList();
+                sections.Add(section);
             }
 
-            // 3. load and fill template
+            // 3. construct body xml
+            var sectionXmls = sections.Select(s => string.Join(Environment.NewLine, s.Select(l => l.GetXml()))).ToList();
+            var bodyXml = string.Join(Environment.NewLine, sectionXmls);
 
-            // 4. write fb2 file
+            // 4. load and fill template
+            var template = File.ReadAllText("fb2template.xml");
+            var fb2Xml = template.Replace(Tokens.Body, bodyXml).Replace(Tokens.BodyNotes, "");
+
+            // 5. write fb2 file
+            File.WriteAllText(fb2Path, fb2Xml);
         }
-
-
 
         private static class Tokens
         {
@@ -59,6 +102,10 @@ namespace LeninSearch.Script.Scripts
             {
                 return $"<a l:href=\"#{id}\" type=\"note\">{text}</a>";
             }
+
+            public const string Body = "[body]";
+
+            public const string BodyNotes = "[body-notes]";
         }
     }
 
