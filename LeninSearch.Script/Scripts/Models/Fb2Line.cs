2e35ac79913e40738a25d931e55ec8eb705573ed
dikt-prole <75211373+dikt-prole@users.noreply.github.com>
2022-04-23 11:00:47 +0300

    general structure of ocr => fb2 script

---
+++ LeninSearch.Script/Scripts/Models/Fb2Line.cs
@@ -0,0 +1,13 @@
+﻿namespace LeninSearch.Script.Scripts.Models
+{
+    public class Fb2Line
+    {
+        public string Text { get; set; }
+        public Fb2LineType Type { get; set; }
+    }
+
+    public enum Fb2LineType
+    {
+        Paragraph, Image
+    }
+}
--- LeninSearch.Script/Scripts/OcrToFb2Script.cs
+++ LeninSearch.Script/Scripts/OcrToFb2Script.cs
@@ -1,15 +1,66 @@
-﻿namespace LeninSearch.Script.Scripts
+﻿using System.Collections.Generic;
+using LeninSearch.Ocr.Model;
+using LeninSearch.Script.Scripts.Models;
+
+namespace LeninSearch.Script.Scripts
 {
     public class OcrToFb2Script : IScript
     {
         public string Id => "ocr-to-fb2";
-        public string Arguments => "ocr-json-folder, fb2-path";
+        public string Arguments => "ocr-book-folder, fb2-path";
         public void Execute(params string[] input)
         {
-            var ocrJsonFolder = input[0];
+            var ocrBookFolder = input[0];
             var fb2Path = input[1];
 
-            // todo: implement ocr => fb2 here
+            // 1. load ocr data
+            var ocrData = OcrData.Load(ocrBookFolder);
+
+            // 2. go through pages and create sections, notes & images
+            var sections = new List<List<Fb2Line>>();
+            var images = new List<string>();
+            var notes = new List<string>();
+            foreach (var page in ocrData.Pages)
+            {
+                var pageParagraphs = new List<Fb2Line>();
+                var pageImages = new List<Fb2Line>();
+            }
+
+            // 3. load and fill template
+
+            // 4. write fb2 file
+        }
+
+
+
+        private static class Tokens
+        {
+            public static class TitleInfo
+            {
+                public const string Genre = "[genre]";
+                public const string BookAuthorFirstName = "[book-author-first-name]";
+                public const string BookAuthorLastName = "[book-author-last-name]";
+                public const string BookAuthorMiddleName = "[book-author-middle-name]";
+                public const string BookId = "[book-id]";
+                public const string BookTitle = "[book-title]";
+                public const string BookAnnotation = "[book-annotation]";
+                public const string BookLanguage = "[book-language]";
+            }
+
+            public static class DocumentInfo
+            {
+                public const string DocAuthorFirstName = "[doc-author-first-name]";
+                public const string DocAuthorLastName = "[doc-author-last-name]";
+                public const string DocId = "[doc-id]";
+                public const string DocVersion = "[doc-version]";
+            }
+
+            public static string CommentMarker(string id, string text)
+            {
+                return $"<a l:href=\"#{id}\" type=\"note\">{text}</a>";
+            }
         }
     }
+
+
 }
