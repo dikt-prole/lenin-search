35447b8be5dd963699f36ac0761edce5fe30df65
dikt-prole <dikt-prole@mail.ru>
2022-12-10 11:57:56 +0300

    Update Resource.designer.cs

--- LeninSearch.Xam/LeninSearch.Xam.Android/Resources/Resource.designer.cs
+++ LeninSearch.Xam/LeninSearch.Xam.Android/Resources/Resource.designer.cs
@@ -22317,31 +22317,34 @@ namespace LeninSearch.Xam.Droid
 			public const int test_custom_background = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int tooltip_frame_dark = 2131165354;
+			public const int tg = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int tooltip_frame_light = 2131165355;
+			public const int tooltip_frame_dark = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int trash = 2131165356;
+			public const int tooltip_frame_light = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int trashred = 2131165357;
+			public const int trash = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int up = 2131165358;
+			public const int trashred = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int white_cursor = 2131165359;
+			public const int up = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int xfootnote = 2131165360;
+			public const int white_cursor = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int xheader = 2131165361;
+			public const int xfootnote = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int xpage = 2131165362;
+			public const int xheader = 2131165362;
+			
+			// aapt resource value: 0x7F0700B3
+			public const int xpage = 2131165363;
 			
 			static Drawable()
 			{
