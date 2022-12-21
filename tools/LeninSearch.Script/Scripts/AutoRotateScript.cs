07e7c426422c8efa4bfdc5a0e69fb6d8940aada7
dikt-prole <dikt-prole@mail.ru>
2022-12-21 15:57:44 +0300

    Update AutoRotateScript.cs

--- tools/LeninSearch.Script/Scripts/AutoRotateScript.cs
+++ tools/LeninSearch.Script/Scripts/AutoRotateScript.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using LeninSearch.Ocr.CV;
 
 namespace LeninSearch.Script.Scripts
 {
@@ -11,12 +15,41 @@ namespace LeninSearch.Script.Scripts
             var imageFolder = input[0];
 
             var imageFiles = Directory.GetFiles(imageFolder, "*.jpg");
+
+            using var backgroundBrush = new SolidBrush(Color.White);
             foreach (var imageFile in imageFiles)
             {
+                using var image = Image.FromFile(imageFile);
+
+                if (image.Width <= image.Height) continue;
+
+                var contours = CvUtil.GetContourRectangles(imageFile, SmoothGaussianArgs.SmallSmooth());
+                var minX = contours.Select(c => c.X).Min();
+                var maxX = contours.Select(c => c.X + c.Width).Max();
+                var minY = contours.Select(c => c.Y).Min();
+                var maxY = contours.Select(c => c.Y + c.Height).Max();
+                var cropRect = new Rectangle(minX, minY, maxX - minX, maxY - minY);
+                var cropBitmap = new Bitmap(cropRect.Width, cropRect.Height);
+                using (var g = Graphics.FromImage(image))
+                {
+                    g.DrawImage(image, 0, 0, cropRect, GraphicsUnit.Pixel);
+                }
                 
+
+                var newWidth = image.Height;
+                var newHeight = cropBitmap.Height * newWidth / cropBitmap.Width;
+                cropBitmap = new Bitmap(cropBitmap, newWidth, newHeight);
+
+                var resultImage = new Bitmap(image.Height, image.Width);
+                using (var g = Graphics.FromImage(resultImage))
+                {
+                    g.FillRectangle(backgroundBrush, 0, 0, resultImage.Width, resultImage.Height);
+
+                    g.DrawImage(cropBitmap, 0, 0);
+                }
+
+                resultImage.Save(imageFile, ImageFormat.Jpeg);
             }
-
-
         }
     }
 }
